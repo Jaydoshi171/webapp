6170ad23d92784fe84e62a5abf57f94700d54d63
Jay Doshi <doshi.jaym@northeastern.edu>
2023-10-04 18:05:40 -0400

    test

--- util/config.js
+++ util/config.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize');
 
+// FOR MYSQL
 const sequelize = new Sequelize(
     'healthCheck',
     'root',
@@ -9,5 +10,20 @@ const sequelize = new Sequelize(
         dialect: 'mysql'
     }
 )
+
+// FOR POSTGRES
+// const sequelize = new Sequelize(
+//     'cloud_db',
+//     'postgres',
+//     'Jay@1998',
+//     {
+//         host: 'localhost',
+//         dialect: 'postgres',
+//         port: '5434'
+//     }
+// )
+
+// const sequelize = new Sequelize(`postgres://postgres:Jay@1998@localhost/cloud_db`);
+
 
 module.exports = sequelize;
