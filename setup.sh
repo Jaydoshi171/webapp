e02051f93c01f3c850ffe09fdcdeb746686c6a3e
Jay Doshi <doshi.jaym@northeastern.edu>
2023-10-18 17:11:16 -0400

    minor changes in workflow

--- setup.sh
+++ setup.sh
@@ -4,9 +4,6 @@ sudo apt-get upgrade -y
 sudo apt install unzip
 sudo apt install nodejs npm -y
 sudo apt install mariadb-server -y
-# echo "USER_PASSWORD: ${MARIA_PASSWORD}"
-# echo "MYSQL_DB_NAME: ${MYSQL_DB_NAME}"
-# echo "USER_PMYSQL_USERASSWORD: ${MYSQL_USER}"
 sudo mysql -e "SET PASSWORD FOR $MARIA_USER@localhost = PASSWORD('$MARIA_PASSWORD');FLUSH PRIVILEGES;"
 printf "%s\n n\n n\n n\n n\n n\n y\n" "$MARIA_PASSWORD" | sudo mysql_secure_installation
 mysql -u "$MARIA_USER" -p"$MARIA_PASSWORD" -Bse "CREATE DATABASE $MYSQL_DB_NAME;"
@@ -17,7 +14,4 @@ sudo mv /home/admin/webapp.zip /home/admin/opt/webapp.zip
 sudo mv /home/admin/users.csv /home/admin/opt/users.csv
 cd opt
 sudo unzip -o webapp.zip
-# cd webapp
 sudo npm i
-# mysql -u "$MARIA_USER" -p"$MARIA_PASSWORD" -Bse "SHOW DATABASES;"
-# sudo npm start
