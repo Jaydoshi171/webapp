8b64aa02803b194c99e24e444616a8c537bb6c31
Jay Doshi <doshi.jaym@northeastern.edu>
2023-11-08 17:20:09 -0500

    made changes in logger messages

--- controllers/assignment_controller.js
+++ controllers/assignment_controller.js
@@ -9,7 +9,7 @@ const getAllAssignments = async (req,res) => {
     try{
         statsd.increment("endpoint.get.allAssignment");
         if (req.body && Object.keys(req.body).length > 0){
-            logger.error("Invalid get request with body included");
+            logger.warn("Invalid get request because of body or url parameters in getAllAssignments function");
             return res.status(400).send();
         }
         const allAssignments = await Assignments.findAll();
@@ -17,12 +17,13 @@ const getAllAssignments = async (req,res) => {
         return res.status(200).json(allAssignments).send();
     }
     catch(error){
-        logger.error(error.message);
+        logger.error(`Error in getAllAssignments function: ${error.message}`);
         return res.status(400).json({error: 'Bad Request'}).send();
     }
 }
 function checkDate(input) {
-    let regex = /[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?([Zz]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?/i;
+    let regex = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?([Zz])$/
+    // let regex = /[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?([Zz]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?/i;
     return regex.test(input);
 }
 
@@ -37,19 +38,19 @@ const createAssignment = async (req,res) => {
         const createdBy = req.account.email;
         const date = new Date(deadline);
         if(!Number.isInteger(points) || !Number.isInteger(num_of_attempts)){
-            logger.error("Bad request: Invalid body parameters");
+            logger.warn("Bad post request because of bad request body in createAssignment function");
             return res.status(400).json({error: 'Bad Request'}).send();
         }
         if (!(typeof name === 'string' || name instanceof String)){
-            logger.error("Bad request: Invalid body parameters");
+            logger.warn("Bad post request because of bad request body in createAssignment function");
             return res.status(400).json({error: 'Bad Request'}).send();
         }
         if(!checkDate(deadline)){
-            logger.error("Bad request: Invalid body parameters");
+            logger.warn("Bad post request because of bad request body in createAssignment function");
             return res.status(400).json({error: 'Bad Request'}).send();
         }
         if(!name || !points || !num_of_attempts || !deadline || !createdBy || Object.keys(req.body).length > 4){
-            logger.error("Bad request: Invalid body parameters");
+            logger.warn("Bad post request because of bad request body in createAssignment function");
             return res.status(400).json({error: 'Bad Request'}).send();
         }
         const new_assignment = await Assignments.create({
@@ -63,7 +64,7 @@ const createAssignment = async (req,res) => {
         return res.status(201).json(new_assignment).send();
     }
     catch(error){
-        logger.error(error.message);
+        logger.error(`Error in createAssignment function: ${error.message}`);
         return res.status(400).json({error: 'Bad Request'}).send();
     }
 }
@@ -72,18 +73,19 @@ const getAssignment = async (req,res) => {
         statsd.increment("endpoint.get.AssignmentByID");
         const id = req.params.id;
         if (req.body && Object.keys(req.body).length > 0){
-            logger.error("Invalid get request with body included");
+            logger.warn("Invalid get request because of body or url parameters in getAssignment function");
             return res.status(400).send();
         }
         const assignment = await Assignments.findOne({where: {id: id}});
         if(!assignment){
-            logger.error("No assignment with id: " + id);
+            logger.warn("No assignment found in getAssignment function with id: " + id);
             return res.status(404).json({error: 'id not found'}).send();
         }
+        logger.info("Assignment requested in getAssignment for id: " + id);
         return res.status(200).json(assignment).send();
     }
     catch(error){
-        logger.error(error.message);
+        logger.error(`Error in getAssignment function: ${error.message}`);
         return res.status(400).json({error: 'Bad Request'}).send();
     }
 }
@@ -93,19 +95,19 @@ const deleteAssignment = async (req,res) => {
         const id = req.params.id;
         const assignment = await Assignments.findOne({where: {id: id}});
         if(!assignment){
-            logger.error("No assignment with id: " + id);
+            logger.warn("No assignment found in getAssignment function with id: " + id);
             return res.status(404).json({error: 'id not found'}).send();
         }
         if(assignment.createdBy!=req.account.email){
-            logger.error("Forbidden delete action by User: " , req.account.email);
+            logger.warn("Forbidden delete action in deleteAssignment function by User: " , req.account.email);
             return res.status(403).json({error: 'Forbidden'}).send();
         }
         await assignment.destroy();
-        logger.info("Assignment deleted for assignment id: " + id);
+        logger.info("Assignment deleted in deleteAssignment function for assignment id: " + id);
         return res.status(204).send();
     }
     catch(error){
-        logger.error(error.message);
+        logger.error(`Error in deleteAssignment function: ${error.message}`);
         return res.status(400).json({error: 'Bad Request'}).send();
     }
 }
@@ -118,28 +120,28 @@ const updateAssignment = async (req,res) => {
         const num_of_attempts = req.body.num_of_attempts;
         const deadline = req.body.deadline;
         if(!Number.isInteger(points) || !Number.isInteger(num_of_attempts)){
-            logger.error("Bad request: Invalid body parameters");
+            logger.warn("Bad put request because of bad request body in updateAssignment function");
             return res.status(400).json({error: 'Bad Request'}).send();
         }
         if (!(typeof name === 'string' || name instanceof String)){
-            logger.error("Bad request: Invalid body parameters");
+            logger.warn("Bad put request because of bad request body in updateAssignment function");
             return res.status(400).json({error: 'Bad Request'}).send();
         }
         if(!checkDate(deadline)){
-            logger.error("Bad request: Invalid body parameters");
+            logger.warn("Bad put request because of bad request body in updateAssignment function");
             return res.status(400).json({error: 'Bad Request'}).send();
         }
         if(!name || !points || !num_of_attempts || !deadline || Object.keys(req.body).length > 4 ){
-            logger.error("Bad request: Invalid body parameters");
+            logger.warn("Bad put request because of bad request body in updateAssignment function");
             return res.status(400).json({error: 'Bad Request'}).send();
         }
         const assignment = await Assignments.findOne({where: {id: id}});
         if(!assignment){
-            logger.error("No assignment with id: " + id);
+            logger.warn("No assignment found in updateAssignment function with id: " + id);
             return res.status(404).json({error: 'id not found'}).send();
         }
         if(assignment.createdBy!=req.account.email){
-            logger.error("Forbidden update action by User: " + req.account.email);
+            logger.warn("Forbidden delete action in updateAssignment function by User: " , req.account.email);
             return res.status(403).json({error: 'Forbidden'}).send();
         }
         assignment.name = name;
@@ -147,11 +149,11 @@ const updateAssignment = async (req,res) => {
         assignment.num_of_attempts = num_of_attempts;
         assignment.deadline = deadline;
         await assignment.save();
-        logger.info("Assignment updated for assignment id: " + id);
+        logger.info("Assignment updated in updateAssignment function for assignment id: " + id);
         return res.status(204).send(); 
     }
     catch(error){
-        logger.error(error.message);
+        logger.error(`Error in updateAssignment function: ${error.message}`);
         return res.status(400).json({error: 'Bad Request'}).send();
     }
 }
