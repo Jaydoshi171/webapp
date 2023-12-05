70ee6694beb91706a03958c9a1c0567c6860e784
Jaydoshi171 <113273437+Jaydoshi171@users.noreply.github.com>
2023-12-04 19:10:28 -0500

    Assignment 10 dev (#42)
    * Update account.spec.js
    
    * Update workflow.yml
    
    * Update workflow.yml
    
    * Update account.spec.js
    
    * minor changes to resolve issues
    
    * added launch template update and autoscale instance refresh
    
    * minor changes
    
    * v1 to v2
    
    * testing
    
    * testing
    
    * minor changes
    
    * testing
    
    * MINOR CHANGES

--- routes/assignment_routes.js
+++ routes/assignment_routes.js
@@ -3,13 +3,13 @@ const assignmentController = require('../controllers/assignment_controller');
 const auth = require('../controllers/authentication');
 
 const router = express.Router();
-router.get('/v2/assignments',auth,assignmentController.getAllAssignments)
-router.post('/v2/assignments',auth,assignmentController.createAssignment)
-router.post('/v2/assignments/:id/submission',auth,assignmentController.postSubmission)
-router.get('/v2/assignments/:id',auth,assignmentController.getAssignment)
-router.delete('/v2/assignments/:id',auth,assignmentController.deleteAssignment)
-router.put('/v2/assignments/:id',auth,assignmentController.updateAssignment)
-router.patch('/v2/assignments',auth,assignmentController.patch1)
-router.patch('/v2/assignments/:id',auth,assignmentController.patch2)
+router.get('/v1/assignments',auth,assignmentController.getAllAssignments)
+router.post('/v1/assignments',auth,assignmentController.createAssignment)
+router.post('/v1/assignments/:id/submission',auth,assignmentController.postSubmission)
+router.get('/v1/assignments/:id',auth,assignmentController.getAssignment)
+router.delete('/v1/assignments/:id',auth,assignmentController.deleteAssignment)
+router.put('/v1/assignments/:id',auth,assignmentController.updateAssignment)
+router.patch('/v1/assignments',auth,assignmentController.patch1)
+router.patch('/v1/assignments/:id',auth,assignmentController.patch2)
 
 module.exports = router
