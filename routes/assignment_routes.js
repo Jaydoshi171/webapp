381e645e9d9075d14c79b2979b813188e75f49a4
Jaydoshi171 <113273437+Jaydoshi171@users.noreply.github.com>
2023-12-05 13:59:57 -0500

    Update assignment_routes.js

--- routes/assignment_routes.js
+++ routes/assignment_routes.js
@@ -3,13 +3,13 @@ const assignmentController = require('../controllers/assignment_controller');
 const auth = require('../controllers/authentication');
 
 const router = express.Router();
-router.get('/demo/assignments',auth,assignmentController.getAllAssignments)
-router.post('/demo/assignments',auth,assignmentController.createAssignment)
-router.post('/demo/assignments/:id/submission',auth,assignmentController.postSubmission)
-router.get('/demo/assignments/:id',auth,assignmentController.getAssignment)
-router.delete('/demo/assignments/:id',auth,assignmentController.deleteAssignment)
-router.put('/demo/assignments/:id',auth,assignmentController.updateAssignment)
-router.patch('/demo/assignments',auth,assignmentController.patch1)
-router.patch('/demo/assignments/:id',auth,assignmentController.patch2)
+router.get('/v3/assignments',auth,assignmentController.getAllAssignments)
+router.post('/v3/assignments',auth,assignmentController.createAssignment)
+router.post('/v3/assignments/:id/submission',auth,assignmentController.postSubmission)
+router.get('/v3/assignments/:id',auth,assignmentController.getAssignment)
+router.delete('/v3/assignments/:id',auth,assignmentController.deleteAssignment)
+router.put('/v3/assignments/:id',auth,assignmentController.updateAssignment)
+router.patch('/v3/assignments',auth,assignmentController.patch1)
+router.patch('/v3/assignments/:id',auth,assignmentController.patch2)
 
 module.exports = router
